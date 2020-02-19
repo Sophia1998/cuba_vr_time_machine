a4f844917fbbdbd274b99259b1687586ddedf067
Sophia1998 <shao.286@osu.edu>
2020-02-19 00:09:19 -0500

    first commit

---
+++ Assets/Scripts/button_controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public void switchToScenePub()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void switchToSceneBeach()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void switchToSceneSquare()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void switchToSceneLobby()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/main_camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main_camera : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * moveSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * moveSpeed;
+        transform.localRotation = transform.localRotation * Quaternion.Euler(-mouseY, 0, 0);
+        transform.localRotation = transform.localRotation * Quaternion.Euler(0, mouseX, 0);
+        
+    }
+}
