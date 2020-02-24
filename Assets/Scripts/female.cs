eada0bda13532a9037f94b90172bfc774f9766d8
fiercewrench <47038910+fiercewrench@users.noreply.github.com>
2020-02-24 03:24:16 -0500

    detect the distance between human and female

---
+++ Assets/Scripts/female.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class female : MonoBehaviour
+{
+    GameObject player;
+    float distance;
+    float radius;
+    GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.Find("Humanoid");
+        cube = GameObject.Find("Cube");
+        distance = (transform.position - player.transform.position).sqrMagnitude;
+        Debug.Log(distance);
+        if(distance < radius)
+        {
+            cube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+    }
+}
